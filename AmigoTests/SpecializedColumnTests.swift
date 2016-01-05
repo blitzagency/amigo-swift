10f57cb3595d9903b3e41b899fb0a7af8cd978cd
Adam Venturella <aventurella@blitzagency.com>
2016-01-05 08:18:24 -0800

    updates tests
    adds UUID as primary key tests
    adjusts case of UUID generation to lowercase

--- AmigoTests/SpecializedColumnTests.swift
+++ AmigoTests/SpecializedColumnTests.swift
@@ -41,7 +41,15 @@ class SpecializedColumnTests: XCTestCase {
 
         let uuidPk = ORMModel(UUIDPKModel.self,
             UUIDField("objId", primaryKey: true){
-                return NSUUID().UUIDString
+                // the input case doesn't actually matter, but
+                // rfc 4122 states that:
+                //
+                // The hexadecimal values "a" through "f" are output as
+                // lower case characters and are case insensitive on input.
+                //
+                // See: https://www.ietf.org/rfc/rfc4122.txt
+                // Declaration of syntactic structure
+                return NSUUID().UUIDString.lowercaseString
             }
         )
 
@@ -65,10 +73,18 @@ class SpecializedColumnTests: XCTestCase {
 
     func testUUIDDefaultValue() {
         let field = UUIDField("objId", primaryKey: true){
-            return NSUUID().UUIDString
+            // the input case doesn't actually matter, but
+            // rfc 4122 states that:
+            //
+            // The hexadecimal values "a" through "f" are output as
+            // lower case characters and are case insensitive on input.
+            //
+            // See: https://www.ietf.org/rfc/rfc4122.txt
+            // Declaration of syntactic structure
+            return NSUUID().UUIDString.lowercaseString
         }
 
-        let value = field.serialize(nil)
+        let value = field.valueOrDefault(nil)
         XCTAssert(value != nil)
     }
 
@@ -76,26 +92,69 @@ class SpecializedColumnTests: XCTestCase {
         let objId = NSUUID().UUIDString
 
         let field = UUIDField("objId", primaryKey: true){
-            return NSUUID().UUIDString
+            // the input case doesn't actually matter, but
+            // rfc 4122 states that:
+            //
+            // The hexadecimal values "a" through "f" are output as
+            // lower case characters and are case insensitive on input.
+            //
+            // See: https://www.ietf.org/rfc/rfc4122.txt
+            // Declaration of syntactic structure
+            return NSUUID().UUIDString.lowercaseString
         }
 
-        let value = field.serialize(objId) as? NSData
-        XCTAssert(value == uuidToBytes(objId))
+        let value = field.valueOrDefault(objId) as! String
+        XCTAssert(value == objId)
     }
 
-    func testUUIDMalformed() {
+    func testUUIDSerializeMalformed() {
         let objId = "ollie"
 
         let field = UUIDField("objId", primaryKey: true){
-            return NSUUID().UUIDString
+            // the input case doesn't actually matter, but
+            // rfc 4122 states that:
+            //
+            // The hexadecimal values "a" through "f" are output as
+            // lower case characters and are case insensitive on input.
+            //
+            // See: https://www.ietf.org/rfc/rfc4122.txt
+            // Declaration of syntactic structure
+            return NSUUID().UUIDString.lowercaseString
         }
 
         let value = field.serialize(objId)
         XCTAssert(value == nil)
     }
 
-    func testUUIDColumn() {
+    func testUUIDSerialize() {
         let objId = NSUUID().UUIDString
+
+        let field = UUIDField("objId", primaryKey: true){
+            // the input case doesn't actually matter, but
+            // rfc 4122 states that:
+            //
+            // The hexadecimal values "a" through "f" are output as
+            // lower case characters and are case insensitive on input.
+            //
+            // See: https://www.ietf.org/rfc/rfc4122.txt
+            // Declaration of syntactic structure
+            return NSUUID().UUIDString.lowercaseString
+        }
+
+        let value = field.serialize(objId) as! NSData
+        XCTAssert(value == uuidToBytes(objId))
+    }
+
+    func testUUIDColumnDeserialize() {
+        // the input case doesn't actually matter, but
+        // rfc 4122 states that:
+        //
+        // The hexadecimal values "a" through "f" are output as
+        // lower case characters and are case insensitive on input.
+        //
+        // See: https://www.ietf.org/rfc/rfc4122.txt
+        // Declaration of syntactic structure
+        let objId = NSUUID().UUIDString.lowercaseString
         let uuid = UUIDModel()
         uuid.objId = objId
 
@@ -103,9 +162,9 @@ class SpecializedColumnTests: XCTestCase {
 
         session.add(uuid)
 
-        if let results = session.query(UUIDModel).all().first{
-            XCTAssert(results.id == 1)
-            XCTAssert(results.objId == objId)
+        if let result = session.query(UUIDModel).all().first{
+            XCTAssert(result.id == 1)
+            XCTAssert(result.objId == objId)
         } else {
             XCTFail()
         }
@@ -113,7 +172,15 @@ class SpecializedColumnTests: XCTestCase {
     }
 
     func testUUIDColumnFilter() {
-        let objId = NSUUID().UUIDString
+        // the input case doesn't actually matter, but
+        // rfc 4122 states that:
+        //
+        // The hexadecimal values "a" through "f" are output as
+        // lower case characters and are case insensitive on input.
+        //
+        // See: https://www.ietf.org/rfc/rfc4122.txt
+        // Declaration of syntactic structure
+        let objId = NSUUID().UUIDString.lowercaseString
         let uuid = UUIDModel()
         uuid.objId = objId
 
@@ -129,30 +196,21 @@ class SpecializedColumnTests: XCTestCase {
             XCTAssert(results.id == 1)
             XCTAssert(results.objId == objId)
         } else {
-            XCTFail()
+            XCTFail("Unable to locate object for filter: 'objId = \(objId)'")
         }
         
     }
 
     func testUUIDAsPrimaryKey() {
-        let objId = NSUUID().UUIDString
         let uuid = UUIDPKModel()
-        //uuid.objId = objId
-
         let session = amigo.session
 
-        //session.add(uuid)
+        session.add(uuid)
 
-//        let query = session
-//            .query(UUIDModel)
-//            .filter("objId = '\(objId)'")
-//
-//        if let results = query.all().first{
-//            XCTAssert(results.id == 1)
-//            XCTAssert(results.objId == objId)
-//        } else {
-//            XCTFail()
-//        }
-
+        if let result = session.query(UUIDPKModel).get(uuid.objId){
+            XCTAssert(result.objId == uuid.objId)
+        } else {
+            XCTFail("Unable to locate object for primary key: \(uuid.objId)")
+        }
     }
 }
